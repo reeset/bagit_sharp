bb7eeb00eb977b5fe0802bcc263d4767834209f7
reese <reese@DESKTOP-M9MK11O>
2017-02-27 00:57:23 -0500

    Add project files.

---
+++ bagit_sharp/BagException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bagit_sharp
+{
+    public class BagException : Exception
+    {
+        public BagException()
+        {
+        }
+
+        public BagException(string message)
+        : base(message)
+    {
+        }
+
+        public BagException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+
+    }
+
+
+    
+}
